b169676256fe2cbc94a163533ea365f22d321d09
Wojciech Mazur <wojciech.mazur.lo@gmail.com>
2024-03-21 15:27:06 +0100

    Pierwszy commit

---
+++ src/webparts/spfx/components/ISpfxProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/spfx/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxWebPartStrings' {
+  const strings: ISpfxWebPartStrings;
+  export = strings;
+}
