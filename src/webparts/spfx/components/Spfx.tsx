3e48af433219e0e9d04479c65eece09ae1052be4
Wojciech Mazur <wojciech.mazur.lo@gmail.com>
2024-03-26 10:08:49 +0100

    Co-authored-by: Jacek Rojek <contact@jacek-rojek.com>

--- src/webparts/spfx/components/Spfx.tsx
+++ src/webparts/spfx/components/Spfx.tsx
@@ -2,7 +2,13 @@ import * as React from 'react';
 import styles from './Spfx.module.scss';
 import type { ISpfxProps } from './ISpfxProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-import { Button, Checkbox, Input, Tab, Tabs } from '@sebgroup/green-react';
+import { Button } from '@mui/material';
+import { styled } from '@mui/system';
+import { Tabs as BaseTabs } from '@mui/base/Tabs';
+import { TabsList as BaseTabsList } from '@mui/base/TabsList';
+import { TabPanel as BaseTabPanel } from '@mui/base/TabPanel';
+import { buttonClasses } from '@mui/base/Button';
+import { Tab as BaseTab, tabClasses } from '@mui/base/Tab';
 
 export default class Spfx extends React.Component<ISpfxProps, {}> {
   public render(): React.ReactElement<ISpfxProps> {
@@ -19,89 +25,112 @@ export default class Spfx extends React.Component<ISpfxProps, {}> {
           <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
           <h2>Witaj {escape(userDisplayName)}!</h2>
           <div>{environmentMessage}</div>
-          <div>
-          <Tabs>
-    <Tab title={'Sekcja 1'}>
-      <>
-        <p>Pytanie 1.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-        <p>Pytanie 2.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-        <p>Pytanie 3.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-        <p>Pytanie 4.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-        <p>Pytanie 5.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-      </>
-    </Tab>
-    <Tab title={'Sekcja 2'}>
-        <>
-          <p>Pytanie 1.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 2.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 3.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 4.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 5.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-        </>
-    </Tab>
-    <Tab title={'Sekcja 3'}>
-        <>
-          <p>Pytanie 1.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 2.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 3.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 4.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 5.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-        </>
-    </Tab>
-    <Tab title={'Sekcja 4'}>
-        <>
-          <p>Pytanie 1.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 2.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 3.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 4.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-          <p>Pytanie 5.</p><Checkbox
-  label="Checkbox input"
-/><Input label="Input"  />
-        </>
-    </Tab>
-  </Tabs>
-          </div>
-          <Button type="submit">Zapisz ankietę</Button>
+          <Tabs defaultValue={0} orientation="vertical">
+      <TabsList>
+        <Tab>Sekcja 1</Tab>
+        <Tab>Sekcja 2</Tab>
+        <Tab>Sekcja 3</Tab>
+      </TabsList>
+      <TabPanel value={0}>First page1</TabPanel>
+      <TabPanel value={1}>Second page</TabPanel>
+      <TabPanel value={2}>Third page</TabPanel>
+    </Tabs>
+          <Button>Button</Button>
         </div>
       </section>
     );
   }
 }
+const blue = {
+  50: '#F0F7FF',
+  100: '#C2E0FF',
+  200: '#80BFFF',
+  300: '#66B2FF',
+  400: '#3399FF',
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+  800: '#004C99',
+  900: '#003A75',
+};
+
+const grey = {
+  50: '#F3F6F9',
+  100: '#E5EAF2',
+  200: '#DAE2ED',
+  300: '#C7D0DD',
+  400: '#B0B8C4',
+  500: '#9DA8B7',
+  600: '#6B7A90',
+  700: '#434D5B',
+  800: '#303740',
+  900: '#1C2025',
+};
+
+const Tab = styled(BaseTab)`
+  font-family: 'IBM Plex Sans', sans-serif;
+  color: white;
+  cursor: pointer;
+  font-size: 0.875rem;
+  font-weight: bold;
+  background-color: transparent;
+  width: 100%;
+  padding: 12px;
+  border: none;
+  border-radius: 7px;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    background-color: ${blue[400]};
+  }
+
+  &:focus {
+    color: #fff;
+    outline: 3px solid ${blue[200]};
+  }
+
+  &.${buttonClasses.focusVisible} {
+    background-color: #fff;
+    color: ${blue[600]};
+  }
+
+  &.${tabClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+
+  &.${tabClasses.selected} {
+    background-color: #fff;
+    color: ${blue[600]};
+  }
+`;
+
+const TabPanel = styled(BaseTabPanel)`
+  width: 100%;
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-size: 0.875rem;
+`;
+
+const Tabs = styled(BaseTabs)`
+  display: flex;
+  gap: 16px;
+  width: 200px;
+`;
+
+const TabsList = styled(BaseTabsList)(
+  ({ theme }) => `
+  min-width: 80px;
+  background-color: ${blue[500]};
+  border-radius: 12px;
+  margin-bottom: 16px;
+  display: flex;
+  padding: 6px;
+  gap: 12px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+  box-shadow: 0px 4px 8px ${theme.palette.mode === 'dark' ? grey[900] : grey[200]};
+  `,
+);
