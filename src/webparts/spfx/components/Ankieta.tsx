d9323d45106320c30be53b66694e5e23c0ba1316
Wojciech Mazur <wojciech.mazur.lo@gmail.com>
2024-04-02 14:24:11 +0200

    Ankieta update

--- src/webparts/spfx/components/Ankieta.tsx
+++ src/webparts/spfx/components/Ankieta.tsx
@@ -2,7 +2,14 @@ import * as React from "react";
 import styles from "./Spfx.module.scss";
 import "./styles.css";
 import type { ISpfxProps } from "./ISpfxProps";
-import { MenuItem, Select } from "@mui/material";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  MenuItem,
+  Select,
+} from "@mui/material";
 
 export default class Ankieta1 extends React.Component<ISpfxProps, {}> {
   public render(): React.ReactElement<ISpfxProps> {
@@ -10,14 +17,49 @@ export default class Ankieta1 extends React.Component<ISpfxProps, {}> {
 
     return (
       <section
-        className={`${styles.spfx} ${hasTeamsContext ? styles.teams : ""}`}
+        className={`${styles.spfx} ${
+          hasTeamsContext ? styles.teams : " shadow"
+        }`}
       >
-        <div className={"p-5 text-4xl flex justify-center"}>
+        <div className={"p-5 m-2 text-4xl flex justify-center "}>
           <div>Ankieta</div>
         </div>
-        <div className={"p-4 text-xl flex justify-center"}>
-          <div>Customer Name: Marcin Lesiak Quality: 21%</div>
+        <div className={"p-5 m-2 justify-center "}>
+          <List>
+            <ListItem>
+              <ListItemText primary="Marcin Lesiak" />
+            </ListItem>
+            <Divider variant="middle" component="li" textAlign="left">
+              Customer Name
+            </Divider>
+
+            <ListItem>
+              <ListItemText primary="AD2137" />
+            </ListItem>
+            <Divider variant="middle" component="li" textAlign="left">
+              Current DD Level
+            </Divider>
+            <ListItem>
+              <ListItemText primary="Programmer" />
+            </ListItem>
+            <Divider variant="middle" component="li" textAlign="left">
+              Review type
+            </Divider>
+            <ListItem>
+              <ListItemText primary="20.03.2024" />
+            </ListItem>
+            <Divider variant="middle" component="li" textAlign="left">
+              QA review started
+            </Divider>
+            <ListItem>
+              <ListItemText primary="19.04.2024" />
+            </ListItem>
+            <Divider variant="middle" component="li" textAlign="left">
+              QA review closed
+            </Divider>
+          </List>
         </div>
+
         <div className="border-b-2 border-sky-500 flex justify-evenly ...">
           <div className={"p-2 m-2"}>
             Czy organizacja może wykazać, że zidentyfikowała właściwe grupy
