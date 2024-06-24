4dbc395f7d810d936ea9b9632039d0e91517e064
Wojciech Mazur <wojciech.mazur.lo@gmail.com>
2024-06-24 13:45:16 +0200

    usunięcie adjustmentsRequired i challengeProcess

--- src/webparts/spfx/components/FeedbackForm.tsx
+++ src/webparts/spfx/components/FeedbackForm.tsx
@@ -29,8 +29,8 @@ type State = {
   qualityChecker: string;
   regulatoryAnalyst: string;
   amountOfFeedbacks: string;
-  adjustmentsRequired: string;
-  challengeProcess: string;
+  // adjustmentsRequired: string;
+  // challengeProcess: string;
 };
 
 export default class FeedbackForm extends React.Component<
@@ -49,8 +49,8 @@ export default class FeedbackForm extends React.Component<
       qualityChecker: "",
       regulatoryAnalyst: "",
       amountOfFeedbacks: "",
-      adjustmentsRequired: "",
-      challengeProcess: "",
+      // adjustmentsRequired: "",
+      // challengeProcess: "",
     };
   }
 
@@ -132,24 +132,6 @@ export default class FeedbackForm extends React.Component<
               </Select>
             </FormControl>
           </div>
-          {/* <div className={"p-2 m-2 justify-center"}> */}
-          {/* <DatePicker
-              label="QA review started"
-              value={qaReviewStarted}
-              onChange={(newValue) => this.handleDateChange(newValue)}
-            /> */}
-          {/* </div> */}
-          {/* <div className={"p-2 m-2 justify-center"}>
-            <TextField
-              multiline
-              maxRows={1}
-              fullWidth
-              id="qa-review-closed"
-              label="QA review closed"
-              value={this.state.qaReviewClosed}
-              onChange={this.handleInputChange("qaReviewClosed")}
-            />
-          </div> */}
           <div className={"p-2 m-2 justify-center"}>
             <FormControl fullWidth>
               <InputLabel id="Review Type">Review Type</InputLabel>
@@ -161,30 +143,58 @@ export default class FeedbackForm extends React.Component<
                 onChange={this.handleInputChange("reviewType")}
               >
                 <MenuItem value="Review">Review</MenuItem>
+                <MenuItem value="Review">Onboarding</MenuItem>
               </Select>
             </FormControl>
           </div>
-          <div className={"p-2 m-2 justify-center"}>
-            <FormControl fullWidth>
-              <InputLabel id="Responsible Team">Responsible Team</InputLabel>
-              <Select
-                labelId="Responsible Team"
-                id="Responsible Team"
-                label="Responsible Team"
-                value={this.state.responsibleTeam}
-                onChange={this.handleInputChange("responsibleTeam")}
-              >
-                <MenuItem value="Remediation">Remediation</MenuItem>
-                <MenuItem value="CB 1 Reviews">CB 1 Reviews</MenuItem>
-                <MenuItem value="CB 2 Reviews">CB 2 Reviews</MenuItem>
-                <MenuItem value="CB 3 Reviews">CB 3 Reviews</MenuItem>
-                <MenuItem value="SF Reviews">SF Reviews</MenuItem>
-                <MenuItem value="IB Reviews">IB Reviews</MenuItem>
-                <MenuItem value="MidCorp Reviews">MidCorp Reviews</MenuItem>
-                <MenuItem value="C&PC">C&PC</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
+          if(this.state.reviewType=="Review")
+          {
+            <div className={"p-2 m-2 justify-center"}>
+              <FormControl fullWidth>
+                <InputLabel id="Responsible Team">Responsible Team</InputLabel>
+                <Select
+                  labelId="Responsible Team"
+                  id="Responsible Team"
+                  label="Responsible Team"
+                  value={this.state.responsibleTeam}
+                  onChange={this.handleInputChange("responsibleTeam")}
+                >
+                  <MenuItem value="Remediation">Remediation</MenuItem>
+                  <MenuItem value="CB 1 Reviews">CB 1 Reviews</MenuItem>
+                  <MenuItem value="CB 2 Reviews">CB 2 Reviews</MenuItem>
+                  <MenuItem value="CB 3 Reviews">CB 3 Reviews</MenuItem>
+                  <MenuItem value="SF Reviews">SF Reviews</MenuItem>
+                  <MenuItem value="IB Reviews">IB Reviews</MenuItem>
+                  <MenuItem value="MidCorp Reviews">MidCorp Reviews</MenuItem>
+                  <MenuItem value="C&PC">C&PC</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          }
+          if(this.state.reviewType=="Onboarding")
+          {
+            <div className={"p-2 m-2 justify-center"}>
+              <FormControl fullWidth>
+                <InputLabel id="Responsible Team">Responsible Team</InputLabel>
+                <Select
+                  labelId="Responsible Team"
+                  id="Responsible Team"
+                  label="Responsible Team"
+                  value={this.state.responsibleTeam}
+                  onChange={this.handleInputChange("responsibleTeam")}
+                >
+                  <MenuItem value="Remediation">Norway</MenuItem>
+                  <MenuItem value="CB 1 Reviews">Denmark</MenuItem>
+                  <MenuItem value="CB 1 Reviews">Finland</MenuItem>
+                  <MenuItem value="CB 1 Reviews">SF Onboardings</MenuItem>
+                  <MenuItem value="CB 1 Reviews">IB Onboardings</MenuItem>
+                  <MenuItem value="CB 1 Reviews">MidCorp Onboardings</MenuItem>
+                  <MenuItem value="CB 1 Reviews">LC Onboardings</MenuItem>
+                  <MenuItem value="CB 1 Reviews">C&PC</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          }
           <div className={"p-2 m-2 justify-center"}>
             <TextField
               multiline
@@ -216,28 +226,6 @@ export default class FeedbackForm extends React.Component<
               label="Amount of feedbacks"
               value={this.state.amountOfFeedbacks}
               onChange={this.handleInputChange("amountOfFeedbacks")}
-            />
-          </div>
-          <div className={"p-2 m-2 justify-center"}>
-            <TextField
-              multiline
-              maxRows={1}
-              fullWidth
-              id="adjustments-required"
-              label="Adjustments required?"
-              value={this.state.adjustmentsRequired}
-              onChange={this.handleInputChange("adjustmentsRequired")}
-            />
-          </div>
-          <div className={"p-2 m-2 justify-center"}>
-            <TextField
-              multiline
-              maxRows={1}
-              fullWidth
-              id="challenge-process"
-              label="Challenge process?"
-              value={this.state.challengeProcess}
-              onChange={this.handleInputChange("challengeProcess")}
             />
           </div>
         </div>
