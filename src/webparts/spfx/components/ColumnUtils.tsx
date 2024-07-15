e109f468c510edbe8b169a6e0f59e19518735cad
Wojciech Mazur <wojciech.mazur.lo@gmail.com>
2024-07-15 10:13:22 +0200

    czyszczenie ColumnUtils ze zbędnych funkcji

--- src/webparts/spfx/components/ColumnUtils.tsx
+++ src/webparts/spfx/components/ColumnUtils.tsx
@@ -1,13 +1,8 @@
 import { IWeb } from "@pnp/sp/webs";
-import { IFieldAddResult } from "@pnp/sp/fields";
 
 export async function getQAData(spWeb: IWeb): Promise<any> {
   try {
     const items: any[] = await spWeb.lists.getByTitle("QA").items();
-
-    items.forEach((item, index) => {
-      //   `Title: ${item.Title}, field_1: ${item.field_1}, field_2: ${item.field_2}, rate: ${item.field_11}`
-    });
 
     const sections = items.reduce((acc, item) => {
       const section = item.Title;
@@ -31,111 +26,5 @@ export async function getQAData(spWeb: IWeb): Promise<any> {
   } catch (error) {
     console.error("Error fetching QA data:", error);
     return {};
-  }
-}
-
-export async function getColumnList(spWeb: IWeb): Promise<string[]> {
-  try {
-    const fields = await spWeb.lists.getByTitle("Dane").fields();
-    return fields.map((field) => field.InternalName);
-  } catch (error) {
-    console.error("Error fetching column list:", error);
-    return [];
-  }
-}
-
-export async function addMultiLineTextColumnToSharePoint(
-  spWeb: IWeb,
-  columnName: string,
-  existingColumns: string[]
-): Promise<void> {
-  if (existingColumns.includes(columnName)) {
-    alert(`Kolumna "${columnName}" już istnieje!`);
-    return;
-  }
-
-  try {
-    const list = spWeb.lists.getByTitle("Dane");
-
-    const textColumn = {
-      description: "This is a multi-line text column",
-      enforceUniqueValues: false,
-      hidden: false,
-      indexed: false,
-      name: columnName,
-      displayName: columnName,
-      text: {
-        allowMultipleLines: true,
-        appendChangesToExistingText: false,
-        linesForEditing: 0,
-        maxLength: 4000,
-      },
-    };
-
-    const fieldAddResult: IFieldAddResult = await list.fields.addMultilineText(
-      textColumn.displayName,
-      {
-        Description: textColumn.description,
-        Indexed: textColumn.indexed,
-        EnforceUniqueValues: textColumn.enforceUniqueValues,
-        AllowHyperlink: true,
-        AppendOnly: textColumn.text.appendChangesToExistingText,
-        NumberOfLines: 6, // Number of lines to display in edit form
-        RestrictedMode: false, // Set to true if you need rich text
-      }
-    );
-
-    console.log("Kolumna została dodana pomyślnie: ", fieldAddResult);
-    alert("Kolumna została dodana pomyślnie!");
-  } catch (error) {
-    console.error("Kolumna nie została dodana pomyślnie!", error);
-    alert("Kolumna nie została dodana pomyślnie!");
-  }
-}
-
-export async function addSingleLineTextColumnToSharePoint(
-  spWeb: IWeb,
-  columnName: string,
-  existingColumns: string[]
-): Promise<void> {
-  if (existingColumns.includes(columnName)) {
-    alert(`Kolumna "${columnName}" już istnieje!`);
-    return;
-  }
-
-  try {
-    const list = spWeb.lists.getByTitle("Dane");
-
-    const textColumn = {
-      description: "This is a single-line text column",
-      enforceUniqueValues: false,
-      hidden: false,
-      indexed: false,
-      name: columnName,
-      displayName: columnName,
-      text: {
-        allowMultipleLines: false,
-        appendChangesToExistingText: false,
-        linesForEditing: 0,
-        maxLength: 255,
-      },
-    };
-
-    const fieldAddResult: IFieldAddResult = await list.fields.addText(
-      textColumn.displayName,
-      {
-        FieldTypeKind: 2, // 2 oznacza typ tekstowy
-        Description: textColumn.description,
-        Indexed: textColumn.indexed,
-        EnforceUniqueValues: textColumn.enforceUniqueValues,
-        MaxLength: textColumn.text.maxLength,
-      }
-    );
-
-    console.log("Kolumna została dodana pomyślnie: ", fieldAddResult);
-    alert("Kolumna została dodana pomyślnie!");
-  } catch (error) {
-    console.error("Kolumna nie została dodana pomyślnie!", error);
-    alert("Kolumna nie została dodana pomyślnie!");
   }
 }
